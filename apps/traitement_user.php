a5f507af9b3bb872a05b368f517d96d745cb7c8e
JohnMoller <johnny.moller.67@gmail.com>
2016-05-26 15:59:13 +0200

    modif'

--- apps/traitement_user.php
+++ apps/traitement_user.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_POST['email'], $_POST['login'], $_POST['password'], $_POST['prenom'], $_POST['nom'], $_POST['sexe'], $_POST['date_inscription'], $_POST['date_naissance'], $_SESSION['actif'], $_SESSION['admin'] $_SESSION['id']))
+if (isset($_POST['action']))
 {
 	if ($_POST['action'] == 'create')
 	{
@@ -54,16 +54,6 @@ if (isset($_POST['email'], $_POST['login'], $_POST['password'], $_POST['prenom']
 			try
 			{
 				$user = $manager->findById($_POST['id']);
-				$user->setEmail($_POST['email']);
-				$user->setLogin($_POST['login']);
-				$user->setPassword($_POST['password']);
-				$user->setPrenom($_POST['prenom']);
-				$user->setNom($_POST['nom']);
-				$user->setSexe($_POST['sexe']);
-				$user->setDateNaissance($_POST['date_naissance']);
-				$user->setDateInscription($_POST['date_inscription']);
-				$user->setActif($_POST['actif']);
-				$user->setAdmin($_POST['admin']);
 				$manager->remove($article);
 				header('Location: index.php');
 				exit;
