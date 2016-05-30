3a5c7d50a7965501229bf9d88a02040fe5b35cbb
philippe-3wa <dossantos.philippe@outlook.com>
2016-05-30 10:01:56 +0200

    laugoutte

--- apps/header.php
+++ apps/header.php
@@ -1,5 +1,7 @@
 <?php
-if (isset($_SESSION['login']))
+if (isset($_SESSION['id']) && isset($_SESSION['admin']))
+	require('views/header_admin.phtml');
+if (isset($_SESSION['id']))
 	require('views/header_in.phtml');
 else
 	require('views/header.phtml');
