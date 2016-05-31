94292a5dcd000f26700ca903fffdf55022bd3dab
JohnMoller <johnny.moller.67@gmail.com>
2016-05-31 11:57:44 +0200

    add

---
+++ apps/produit_add.php
@@ -0,0 +1,19 @@
+<?php
+$manager = new SousCategorieManager($link);
+	try
+	{
+		$sous_categories = $manager->findAll();
+		$count = 0;
+		$max = sizeof($sous_categories);
+		while ($count < $max)
+		{
+			$sous_categorie = $sous_categories[$count];
+			require('views/admin_bloc_produit_add_liste_sous_categorie.phtml');
+			$count++;
+		}
+	}
+	catch (Exception $exception)
+	{
+		$error = $exception->getMessage();
+	}
+?>
