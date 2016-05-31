e1ec9c193989daedac6c603c21939e5c5a6edb0f
philippe-3wa <dossantos.philippe@outlook.com>
2016-05-31 13:54:33 +0200

    menu

---
+++ apps/header_menu_categorie.php
@@ -0,0 +1,39 @@
+<?php
+
+$manager = new CategorieManager($link);
+	try
+	{
+		$categories = $manager->findAll();
+		$count = 0;
+		$max = sizeof($categories);
+		while ($count < $max)
+		{
+			$categorie = $categories[$count];
+			require('views/header_menu_categorie.phtml');
+			$count++;
+		}
+	}
+	catch (Exception $exception)
+	{
+		$error = $exception->getMessage();
+	}
+
+
+	$manager = new SousCategorieManager($link);
+	try
+	{
+		$sous_categories = $manager->findAllGroup();
+		$count = 0;
+		$max = sizeof($sous_categories);
+		while ($count < $max)
+		{
+			$sous_categorie = $sous_categories[$count];
+			require('views/header_menu_sous_categorie.phtml');
+			$count++;
+		}
+	}
+	catch (Exception $exception)
+	{
+		$error = $exception->getMessage();
+	}
+?>
