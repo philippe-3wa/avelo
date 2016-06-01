6ff084dd0d58c62cf1d542950c671aad78960d04
philippe-3wa <dossantos.philippe@outlook.com>
2016-06-01 14:25:34 +0200

    preparation panier

--- apps/traitement_panier.php
+++ apps/traitement_panier.php
@@ -1,12 +1,12 @@
 <?php
 if (isset($_POST['action']))
 {
-	if ($_POST['action'] == 'create')
+	if ($_POST['action'] == 'ajout_panier')
 	{
-		$manager = new ProduitManager($link);
+		$manager = new PanierManager($link);
 		try
 		{
-			$produit = $manager->create($_POST);
+			$panier = $manager->create($_POST);
 			header('Location: index.php');
 			exit;
 		}
@@ -15,46 +15,7 @@ if (isset($_POST['action']))
 			$error = $exception->getMessage();
 		}
 	}
+
+
 }
-else if ($_POST['action'] == 'update')
-	{
-		if (isset($_SESSION['user']))
-		{
-			$manager = new ProduitManager($link);
-			try
-			{
-				$produit = $manager->findById($_POST['id']);
-				$produit->setDate($_POST['date']);
-				$produit->setNbrProduits($_POST['nbr_produits']);
-				$produit->setStatu($_POST['statu']);
-				$produit->setPrix($_POST['prix']);
-				$produit->setPoids($_POST['poids']);
-				$produit->setIdUser($_POST['id_user']);
-				header('Location: index.php');
-				exit;
-			}
-			catch (Exception $exception)
-			{
-				$error = $exception->getMessage();
-			}
-		}
-	}
-	else if ($_POST['action'] == 'remove')
-	{
-		if (isset($_SESSION['user']))
-		{
-			$manager = new produitManager($link);
-			try
-			{
-				$produit = $manager->findById($_POST['id']);
-				$manager->remove($produit);
-				header('Location: index.php');
-				exit;
-			}
-			catch (Exception $exception)
-			{
-				$error = $exception->getMessage();
-			}
-		}
-	}
 ?>
