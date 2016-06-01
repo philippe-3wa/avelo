0345b8ebd14c3028c923d829fcd004acaa76088b
Praxo <horus.shark73@gmail.com>
2016-06-01 16:20:42 +0200

    acces page id

--- apps/produit.php
+++ apps/produit.php
@@ -1,18 +1,23 @@
 <?php
-$manager = new ProduitManager($link);
-	try
-	{
-		$id = intval($_GET['id']);
-		$produit = $manager->findById($id);
-
-		require('views/produit.phtml');
-
-		
-	}
-	catch (Exception $exception)
-	{
-		$error = $exception->getMessage();
-	}
-
-	
+if (!isset($_GET['id']))
+{
+	require('views/error.phtml');
+	exit;
+}
+else
+{	
+	$manager = new ProduitManager($link);
+		try
+		{
+			$id = intval($_GET['id']);
+			$produit = $manager->findById($id);
+			require('views/produit.phtml');
+			
+		}
+		catch (Exception $exception)
+		{
+			$error = $exception->getMessage();
+		}
+}
 ?>
+
