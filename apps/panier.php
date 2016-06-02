68678407b064911be4be4dccb375dc87ed4ef9da
philippe-3wa <dossantos.philippe@outlook.com>
2016-06-02 10:12:04 +0200

    panier

--- apps/panier.php
+++ apps/panier.php
@@ -1,3 +1,27 @@
 <?php
-	require('views/panier.phtml');
+$manager = new PanierManager($link);
+	try
+	{
+		$user_id = $_SESSION['id'];
+		$panier = $manager->findByIdUserActif($user_id);
+		if ($panier)
+		{
+			$prix = $panier->getPrix();
+			$nombre_produits = $panier->getNbrProduits();
+			$poids = $panier->getPoids();
+		}
+		else
+		{
+			$prix = 0;
+			$nombre_produits = 0;
+			$poids = 0;
+		}
+		require('views/panier.phtml');
+
+	}
+	catch (Exception $exception)
+	{
+		$error = $exception->getMessage();
+	}
+
 ?>
