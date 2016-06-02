17c2c1fd17d9923ea57e3314cdc78055ac471e2c
Praxo <horus.shark73@gmail.com>
2016-06-02 13:59:50 +0200

    ajout avis dans page produit

--- apps/produit.php
+++ apps/produit.php
@@ -12,6 +12,15 @@ else
 			$id = intval($_GET['id']);
 			$produit = $manager->findById($id);
 			require('views/produit.phtml');
+			$liste_avis = $produit->getListeAvis();
+			$count = 0;
+			$max = sizeof($liste_avis);
+			while ($count < $max)
+			{
+				$avis = $liste_avis[$count];
+				require('views/avis.phtml');
+				$count++;
+			}
 			
 		}
 		catch (Exception $exception)
