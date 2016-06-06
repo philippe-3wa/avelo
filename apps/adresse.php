b8b5ad4f649721f1ce5e61e1b60796d439e32f5b
philippe-3wa <dossantos.philippe@outlook.com>
2016-06-06 16:24:52 +0200

    adresses

--- apps/adresse.php
+++ apps/adresse.php
@@ -1,3 +1,15 @@
 <?php
-require('views/adresse.phtml');
+if (!isset($_SESSION['id']))
+	require('views/error.phtml');
+else
+	require('views/adresse.phtml');
+
+if (isset($_GET['option']))
+{
+	$option = $_GET['option'];
+
+	if ($option == "add_adresse")
+		require ('views/adresse_add.phtml');
+}
+	
 ?>
