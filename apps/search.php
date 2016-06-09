960a9ec6c0b3e1c352fcdec8733ec30fdec34d89
philippe-3wa <dossantos.philippe@outlook.com>
2016-06-09 16:41:24 +0200

    recherche

--- apps/search.php
+++ apps/search.php
@@ -1,17 +1,14 @@
 <?php
 if (!isset($_GET['motcle']))
-{
-	require('views/search_nok.phtml');
-	exit;
-}
+	require('views/search_ok.phtml');
 else
 {
-	$motcle = htmlentities($_GET['motcle']);
-	$manager = new ProduitManager($link);
-	$produit = $manager->findAllSearch($motcle);
-	if ($produit)
-		require('views/search_ok.phtml');
-	else
+
+$motcle = htmlentities($_GET['motcle']);
+$manager = new ProduitManager($link);
+$produit = $manager->findAllSearch($motcle);
+if ($produit)
 		require('views/search_nok.phtml');
 }
+
 ?>
