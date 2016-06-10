921ccfea3d9505701d74af4cfd15d3c58de5f361
philippe-3wa <dossantos.philippe@outlook.com>
2016-06-10 09:11:53 +0200

    recherche

--- apps/search.php
+++ apps/search.php
@@ -8,18 +8,19 @@ $motcle = htmlentities($_GET['motcle']);
 $manager = new ProduitManager($link);
 $produits = $manager->findAllSearch($motcle);
 
-if (!$produits)
+if ((!$produits)|| ($motcle==""))
 {
 	require('views/search_nok.phtml');
 }
 else
 {
+	require('views/search_ok.phtml');
 	$count = 0;
 	$max = sizeof($produits);
 	while ($count < $max)
 	{
 		$produit = $produits[$count];
-		require('views/search_ok.phtml');
+		require('views/home_fiche_produit.phtml');
 		$count++;
 	}
 }
